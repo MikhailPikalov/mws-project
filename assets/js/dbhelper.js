e8b96ca54814ddd8967478f288db950c9d225ac3
Mikhail.Pikalov <mikhail.pikalov@gmail.com>
2018-02-24 02:07:19 +0500

    Fixed paths to assets
    Refactored html-files

--- assets/js/dbhelper.js
+++ assets/js/dbhelper.js
@@ -185,7 +185,7 @@ class DBHelper {
      * Restaurant image URL.
      */
     static imageUrlForRestaurant(restaurant) {
-        return (`/img/${restaurant.photograph}`);
+        return (`/assets/img/${restaurant.photograph}`);
     }
 
 
